52c24dda6f7ced81427a480dc9ec05795d2a7d3c
xiaochangyan2018 <544251865@qq.com>
2026-03-08 09:59:07 +0800

    add switch and iconButton

--- XchyUIDemo/obj/Debug/net8.0/XchyUIDemo.AssemblyInfo.cs
+++ XchyUIDemo/obj/Debug/net8.0/XchyUIDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XchyUIDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78215ed77ad5e135ffb85e2db96e2c2a34941deb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bca0aebd476f7ff2a2c3624218f011155dffd6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("XchyUIDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XchyUIDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
